68f5d6e02d4b8aeae67b81568096f7a9868e12ac
Matt Lindstrom <mattsbmws@gmail.com>
2014-02-03 13:31:07 -0500

    Readding Challenge_1

---
+++ Challenge_1/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string userName;
+  cout << "What is your name?\n";
+  cin >> userName;
+  cout << "Hello " << userName << "!\n";
+  return 0;
+}
