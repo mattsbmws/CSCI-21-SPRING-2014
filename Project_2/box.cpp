90e8cf6006fc345ea18bf5e61a2a24a4cd8baba2
Matt Lindstrom <mattsbmws@gmail.com>
2014-04-07 12:50:37 -0400

    Completed Challenge 17

--- Project_2/box.cpp
+++ Project_2/box.cpp
@@ -12,17 +12,6 @@
 
 #include "box.h"
 
-/*
-private data members: boxNumber (unsigned int), boxColor (string), scratch (Prize), prizes (Prize*, for dynamic array), prizeCapacity (unsigned int), prizeCount (unsigned int)
-            public default constructor: initial values boxNumber (0), boxColor ("NO COLOR"), prizeCapacity (5), prizeCount(0); in the definition, prizes array must be initialized to match prizeCapacity
-            public overloaded constructor: parameters for boxNumber, boxColor, prizeCapacity; in the definition, prizes array must be initialized to match prizeCapacity
-            public destructor: free memory associated with prizes
-            public accessors/gets AND mutators/sets for boxNumber, boxColor
-            public accessor/get ONLY for data members prizeCapacity, prizeCount
-            public addPrize: parameters prize (Prize), return value (bool); place prize in prizes array if there is space and return true, else return false
-            public getPrize: parameters index (unsigned int), return value Prize&; return a Prize if index is valid, else return scratch (data member declared in class header)
-            public removePrize: parameters index (unsigned int), return value Prize; remove and return Prize if index is valid, else return scratch (data member declared in class header)
-*/
 
 Box::Box()
 :boxNumber(0), boxColor("NO COLOR"), prizeCapacity(5), prizeCount(0) {
